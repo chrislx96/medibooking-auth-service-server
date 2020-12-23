bbbcea921c7f58f452476af715cc9459cc83365a
xlli3 <xlli3@student.unimelb.edu.au>
2020-12-23 23:09:52 +1100

    Rename files.

--- src/main/java/com/medibooking/authenticationserver/AuthenticationServerApplication.java
+++ src/main/java/com/medibooking/authenticationserver/AuthenticationServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationserverApplication {
+public class AuthenticationServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationserverApplication.class, args);
+		SpringApplication.run(AuthenticationServerApplication.class, args);
 	}
 
 }
