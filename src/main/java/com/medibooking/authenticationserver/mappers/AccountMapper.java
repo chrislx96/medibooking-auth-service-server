9d665148c9829ec948e9c4f74228ab2cdda62bd1
xlli3 <xlli3@student.unimelb.edu.au>
2020-12-25 15:30:16 +1100

    Fix bug in AccountMapper.

--- src/main/java/com/medibooking/authenticationserver/mappers/AccountMapper.java
+++ src/main/java/com/medibooking/authenticationserver/mappers/AccountMapper.java
@@ -12,15 +12,15 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface AccountMapper {
 
-    @Mapping(source = "username", target = "username")
-    @Mapping(source = "encodedPassword", target = "encodedPassword")
-    @Mapping(source = "id", target = "id")
+//    @Mapping(source = "username", target = "username")
+//    @Mapping(source = "encodedPassword", target = "encodedPassword")
+//    @Mapping(source = "id", target = "id")
     AccountGetDto fromEntity(Account account);
 
-    @Mapping(source = "username", target = "username")
-    @Mapping(source = "encodedPassword", target = "encodedPassword")
+//    @Mapping(source = "username", target = "username")
+//    @Mapping(source = "encodedPassword", target = "encodedPassword")
     Account toEntity(AccountPostDto accountPostDto);
 
-    @Mapping(source = "encodedPassword", target = "encodedPassword")
+//    @Mapping(source = "encodedPassword", target = "encodedPassword")
     void copy(AccountPutDto accountPutDto, @MappingTarget Account account);
 }
