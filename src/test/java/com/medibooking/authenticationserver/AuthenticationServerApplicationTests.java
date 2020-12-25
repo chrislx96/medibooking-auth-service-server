1067583d5b9552efd75f934980560dd659ae6695
xlli3 <xlli3@student.unimelb.edu.au>
2020-12-25 15:34:47 +1100

    delete java unit test.

--- src/test/java/com/medibooking/authenticationserver/AuthenticationServerApplicationTests.java
+++ src/test/java/com/medibooking/authenticationserver/AuthenticationServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AuthenticationServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
